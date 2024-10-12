c30d7fd834e8025d9b5e903fffaef983ec39a505
Alexander Pletzer <alexander@gokliya.net>
2024-10-12 17:05:29 +1300

    need to be able to set Fr

--- src/shipWake.py
+++ src/shipWake.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 from scipy.integrate import quad_vec
+import vtk
 
 class ShipWake(object):
     
@@ -41,6 +42,82 @@ class ShipWake(object):
             return integrand(k).imag
         
         return quad_vec(realIntegrand, kg + eps, np.inf)[0] + 1j*quad_vec(imagIntegrand, kg + eps, np.inf)[0]
+    
+    
+    def show(self, xx, yy, zz):
+
+        ny1, nx1 = xx.shape
+        numPoints = nx1 * ny1
+        fMin = zz.min()
+        fMax = zz.max()
+
+        # create the pipeline objects
+        data = vtk.vtkDoubleArray()
+        coords = vtk.vtkDoubleArray()
+        pts = vtk.vtkPoints()
+        grid = vtk.vtkStructuredGrid()
+        dataMapper = vtk.vtkDataSetMapper()
+        dataActor = vtk.vtkActor()
+        lut = vtk.vtkLookupTable()
+        cbar = vtk.vtkScalarBarActor()
+
+        # build the lookup table
+        ncolors = 64
+        lut.SetNumberOfColors(ncolors)
+        for i in range(ncolors):
+            x = 0.5*i*np.pi/(ncolors - 1.)
+            r = np.sin(3*x)**2
+            g = np.sin(1*x)**2
+            b = np.sin(5*x)**2
+            a = 1.0 # opacity
+            lut.SetTableValue(i, r, g, b, a)
+        lut.SetTableRange(fMin, fMax)
+        cbar.SetLookupTable(lut)
+        dataMapper.SetUseLookupTableScalarRange(1)
+
+        # construct the grid
+        grid.SetDimensions(nx1, ny1, 1)
+
+        xyz = np.empty((numPoints, 3), float)
+        xyz[:, 0] = xx.flat
+        xyz[:, 1] = yy.flat
+        xyz[:, 2] = zz.flat
+        coords.SetNumberOfComponents(3)
+        coords.SetNumberOfTuples(numPoints)
+        coords.SetVoidArray(xyz, 3*numPoints, 1)
+
+        # set the data
+        data.SetNumberOfComponents(1) # scalar
+        data.SetNumberOfTuples(numPoints)
+        # set the data to some function of the coordinate
+        save = 1
+        data.SetVoidArray(zz, numPoints, save)
+
+
+        # connect
+        pts.SetNumberOfPoints(numPoints)
+        pts.SetData(coords)
+        grid.SetPoints(pts)
+        grid.GetPointData().SetScalars(data)
+        dataMapper.SetInputData(grid)
+        dataActor.SetMapper(dataMapper)
+        dataMapper.SetLookupTable(lut)
+
+        # show
+        ren = vtk.vtkRenderer()
+        renWin = vtk.vtkRenderWindow()
+        renWin.AddRenderer(ren)
+        iren = vtk.vtkRenderWindowInteractor()
+        iren.SetRenderWindow(renWin)
+        # add the actors to the renderer, set the background and size
+        ren.AddActor(dataActor)
+        ren.AddActor(cbar)
+        ren.SetBackground(0.5, 0.5, 0.5)
+        renWin.SetSize(900, 600)
+        iren.Initialize()
+        renWin.Render()
+        iren.Start()
+       
             
     
 if __name__ == '__main__':
@@ -48,3 +125,9 @@ if __name__ == '__main__':
         x, y = -20., 1.2
         zeta = sw.compute(x=-20., y=1.2, froude=0.5)
         print(f'(x,y)=({x:.2f}, {y:.2f} zeta = {zeta}')
+        
+        x = np.linspace(-140, 10., 21)
+        y = np.linspace(0., 50., 11)
+        xx, yy = np.meshgrid(x, y)
+        zz = sw.compute(x=xx, y=yy, froude=0.5)
+        print(f'zz.real min/max = {zz.real.min()}/{zz.real.max()}')
